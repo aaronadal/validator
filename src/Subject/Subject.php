cccae16ae8910d07f54ca10516fd0d95996cd1bc
Aarón Nadal <aaronadal.dev@gmail.com>
2017-07-12 13:33:38 +0200

    Extras in subject
    Implemented the ability to add extra pieces of information in subjects.

--- src/Subject/Subject.php
+++ src/Subject/Subject.php
@@ -7,9 +7,9 @@ use Aaronadal\Validator\Validator\DataProvider\ArrayDataProvider;
 use Aaronadal\Validator\Validator\DataProvider\DataProviderInterface;
 use Aaronadal\Validator\Validator\DataSetter\ArrayDataSetter;
 use Aaronadal\Validator\Validator\DataSetter\DataSetterInterface;
+use Aaronadal\Validator\Validator\ErrorCollector\ArrayMessageBag;
 use Aaronadal\Validator\Validator\ErrorCollector\DefaultErrorCollector;
 use Aaronadal\Validator\Validator\ErrorCollector\ErrorCollectorInterface;
-use Aaronadal\Validator\Validator\ErrorCollector\MessageBagInterface;
 
 /**
  * @author Aarón Nadal <aaronadal.dev@gmail.com>
@@ -38,23 +38,31 @@ class Subject implements SubjectInterface
     private $errorCollector = null;
 
     /**
+     * @var ArrayMessageBag $extras
+     */
+    private $extras;
+
+    /**
      * Creates a new Subject instance.
      *
      * @param string                       $id             The unique ID of the subject.
      * @param DataProviderInterface|null   $dataProvider   The data provider.
      * @param DataSetterInterface|null     $dataSetter     The data setter.
      * @param ErrorCollectorInterface|null $errorCollector The error collector.
+     * @param ArrayMessageBag|null         $extras         The extras collection.
      */
     public function __construct(
         $id,
         DataProviderInterface $dataProvider = null,
         DataSetterInterface $dataSetter = null,
-        ErrorCollectorInterface $errorCollector = null
+        ErrorCollectorInterface $errorCollector = null,
+        ArrayMessageBag $extras = null
     ) {
         $this->id             = $id;
         $this->dataProvider   = $dataProvider ?: new ArrayDataProvider();
         $this->dataSetter     = $dataSetter ?: new ArrayDataSetter();
         $this->errorCollector = $errorCollector ?: new DefaultErrorCollector();
+        $this->extras         = $extras ?: new ArrayMessageBag();
     }
 
     /**
@@ -257,5 +265,73 @@ class Subject implements SubjectInterface
     public function anyWarning($strict = true)
     {
         return $this->errorCollector->anyWarning($strict);
+    }
+
+    /**
+     * Adds a piece of extra data to this subject.
+     *
+     * @param string $key
+     * @param mixed  $extra
+     */
+    public function putExtra($key, $extra)
+    {
+        $this->extras->put($key, $extra);
+    }
+
+    /**
+     * Retrieves an extra previously defined. If not defined, returns null.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function getExtra($key)
+    {
+        return $this->extras->get($key);
+    }
+
+    /**
+     * Returns all the extras.
+     *
+     * @return array
+     */
+    public function allExtras()
+    {
+        return $this->extras->all();
+    }
+
+    /**
+     * Sets the extras as an array.
+     *
+     * @param array $extras
+     */
+    public function setExtras($extras = [])
+    {
+        $this->extras->setMessages($extras);
+    }
+
+    /**
+     * ¿Is an extra defined?
+     *
+     * @param string $key
+     * @param bool   $strict
+     *
+     * @return bool
+     */
+    public function hasExtra($key, $strict = true)
+    {
+        return $this->extras->has($key, $strict);
+    }
+
+    /**
+     * ¿Has any extra defined?
+     *
+     * @param bool $strict
+     *
+     * @return bool
+     */
+    public function anyExtra($strict = true)
+    {
+        return $this->extras->any($strict);
     }
 }
--- src/Subject/SubjectInterface.php
+++ src/Subject/SubjectInterface.php
@@ -19,4 +19,54 @@ interface SubjectInterface extends ValidableInterface, DataProviderInterface, Da
      */
     public function getId();
 
+    /**
+     * Adds a piece of extra data to this subject.
+     *
+     * @param string $key
+     * @param mixed  $extra
+     */
+    public function putExtra($key, $extra);
+
+    /**
+     * Retrieves an extra previously defined. If not defined, returns null.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function getExtra($key);
+
+    /**
+     * Returns all the extras.
+     *
+     * @return array
+     */
+    public function allExtras();
+
+    /**
+     * Sets the extras as an array.
+     *
+     * @param array $extras
+     */
+    public function setExtras($extras = []);
+
+    /**
+     * ¿Is an extra defined?
+     *
+     * @param string $key
+     * @param bool   $strict
+     *
+     * @return bool
+     */
+    public function hasExtra($key, $strict = true);
+
+    /**
+     * ¿Has any extra defined?
+     *
+     * @param bool $strict
+     *
+     * @return bool
+     */
+    public function anyExtra($strict = true);
+
 }
